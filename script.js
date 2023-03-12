a091d85113c5ac1d3eb4c49d134e67d2e34a65e0
Andrew Miller <andrew.william.miller7@gmail.com>
2023-03-12 16:27:39 +1300

    Simplified randomization.

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-let playButton = document.querySelector('#play-button')
-let squareOne = document.querySelector('#square-1')
-let squareTwo = document.querySelector('#square-2')
-let squareThree = document.querySelector('#square-3')
-let squareFour = document.querySelector('#square-4')
-let level = 0
-var index = -1
+let playButton = document.querySelector('#play-button');
+let squareOne = document.querySelector('#square-1');
+let squareTwo = document.querySelector('#square-2');
+let squareThree = document.querySelector('#square-3');
+let squareFour = document.querySelector('#square-4');
+let level = 0;
+var index = -1;
 var storedIndices = [
 ]
 var requiredNumber = ""
 
 function generateIndex () {
-    var generatedNumber = Math.floor(Math.random() * (5-1) + 1)
+    var generatedNumber = Math.floor(Math.random() * 4) + 1
 
     if (generatedNumber === 0) {
-        generatedNumber = Math.floor(Math.random() * (5-1) + 1)
+        generatedNumber = Math.floor(Math.random() * 4) + 1
     } else {
-        // console.log(generatedNumber)
+        console.log(generatedNumber)
         return generatedNumber
     }
 }
