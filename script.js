5bd56e098bab5c52f1c308e7d98173d8029aaf63
Andrew Miller <andrew.william.miller7@gmail.com>
2023-03-12 16:58:52 +1300

    Replaced for with a recursive function to fix timing.

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ var storedIndices = [
 ]
 var requiredNumber = ""
 
-function generateIndex () {
+function generateIndex() {
     var generatedNumber = Math.floor(Math.random() * 4) + 1
 
     if (generatedNumber === 0) {
@@ -20,27 +20,55 @@ function generateIndex () {
     }
 }
 
-function startRound () {
+function startRound() {
 
     storedIndices.push({
         number: generateIndex(),
-        index: index+= 1
+        index: index += 1
     })
 
     var numbersArray = storedIndices.map(item => item.number);
     var numbersIndex = storedIndices.map(item => item.index);
 
-    for (let i = 0; i < storedIndices.length; i++) {
-
-        document.querySelector(`#square-${numbersArray[i]}`).classList.toggle(`square-${numbersArray[i]}`)
-
+    // recursion a function that calls itself
+    const processHighlight = (i) => {
+        document.querySelector(`#square-${numbersArray[i]}`).classList.toggle(`square-${numbersArray[i]}`);
 
         setTimeout(() => {
-            document.querySelector(`#square-${numbersArray[i]}`).classList.toggle(`square-${numbersArray[i]}`)
-        }, 500 * numbersIndex[i]);
-        console.log("End of iteration, i has value of: " + i);
-}}
+            document.querySelector(`#square-${numbersArray[i]}`).classList.toggle(`square-${numbersArray[i]}`);
+            i++; // increment the value of i or you'll have an infinite loop (which will crash with array out of bounds error)
+            if (i < storedIndices.length) {
+                processHighlight(i);
+            }
+        }, 500);
+    }
+    processHighlight(0);
+}
 
 playButton.addEventListener('click', () => {
     startRound()
 })
+
+
+
+
+
+
+
+
+
+
+// old code for reference
+/*
+    // for (let i = 0; i < storedIndices.length; i++) {
+    //     console.log("query selector value: ~" + `#square-${numbersArray[i]}~`);
+    //     document.querySelector(`#square-${numbersArray[i]}`).classList.toggle(`square-${numbersArray[i]}`)
+
+
+    //     setTimeout(() => {
+    //         document.querySelector(`#square-${numbersArray[i]}`).classList.toggle(`square-${numbersArray[i]}`)
+    //     }, 500 * numbersIndex[i]);
+    //     console.log("End of iteration, i has value of: " + i);
+    // }
+
+*/
